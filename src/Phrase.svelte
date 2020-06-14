cede604adfb6993abbadbd55a592729780ea0122
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-06-14 23:18:37 +0900

    Add translate link

--- src/Phrase.svelte
+++ src/Phrase.svelte
@@ -4,6 +4,8 @@
     meaning: "",
     example: ""
   };
+
+  $: queryParam = encodeURIComponent(phrase.example);
 </script>
 
 <style>
@@ -11,6 +13,8 @@
     box-shadow: rgba(0, 0, 0, 0.2) 2px 2px 6px 3px;
     border-radius: 10px;
     padding: 1em;
+    max-width: 420px;
+    margin: 0 auto;
   }
 
   .phrase {
@@ -27,12 +31,22 @@
     font-size: 1.4rem;
     font-style: italic;
     font-weight: lighter;
-    color: #666;
+    color: #444;
   }
 </style>
 
 <section>
   <p class="phrase">{phrase.body}</p>
   <p class="meaning">{phrase.meaning}</p>
-  <p class="example">{phrase.example}</p>
+  <p class="example">
+    {phrase.example} (
+    <a
+      href="https://translate.google.com/?sl=en&tl=ja&text={queryParam}"
+      target="_blank"
+      rel="noopener">
+      Translate
+    </a>
+    )
+  </p>
+
 </section>
