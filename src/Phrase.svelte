3b3c7793c1cd83277d361601ab20ff197ef5fd56
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-06-14 23:08:46 +0900

    Add phrase component

---
+++ src/Phrase.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let phrase = {
+    body: "",
+    meaning: "",
+    example: ""
+  };
+</script>
+
+<style>
+  section {
+    box-shadow: rgba(0, 0, 0, 0.2) 2px 2px 6px 3px;
+    border-radius: 10px;
+    padding: 1em;
+  }
+
+  .phrase {
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+
+  .meaning {
+    font-size: 1.4rem;
+    color: #3a3;
+  }
+
+  .example {
+    font-size: 1.4rem;
+    font-style: italic;
+    font-weight: lighter;
+    color: #666;
+  }
+</style>
+
+<section>
+  <p class="phrase">{phrase.body}</p>
+  <p class="meaning">{phrase.meaning}</p>
+  <p class="example">{phrase.example}</p>
+</section>
