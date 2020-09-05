64ff58f11f67ec87bff10b6f754c09a49f6ea29a
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-09-05 21:39:11 +0900

    Change face

--- src/FaceButton.svelte
+++ src/FaceButton.svelte
@@ -5,50 +5,68 @@
 <style type="text/scss">
   .face {
     position: relative;
-    background-color: #ccc;
     font-size: 4px;
     width: 12.5em;
     height: 12.5em;
     border-radius: 100%;
     cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    .eyes {
+      position: absolute;
+      left: 2.6em;
+      top: 3.6em;
+    }
+
+    .eye {
+      position: absolute;
+      background-color: black;
+      width: 1.2em;
+      height: 1.9em;
+      border-radius: 100%;
+
+      &.left {
+        left: 0em;
+      }
+
+      &.right {
+        left: 2.8em;
+      }
+    }
+
+    .mouth {
+      position: absolute;
+      background-color: black;
+      top: 7em;
+      left: 3.2em;
+      width: 3.2em;
+      height: 0.5em;
+      border-radius: 2.2em 2.2em 2em 2em;
+    }
+
+    &:not(.selected) {
+      background-color: #ccc;
+
+      .mouth {
+        left: 4.2em;
+        top: 7.3em;
+        width: 1.2em;
+        height: 0.5em;
+      }
+    }
 
     &.selected {
       background-color: #ffdd40;
+
+      .eye {
+        animation: eyes-blink 4.2s infinite alternate;
+      }
+      .mouth {
+        width: 3.2em;
+        height: 1.9em;
+        animation: mounth-shape 2s infinite alternate;
+      }
     }
-  }
-
-  .eyes {
-    position: absolute;
-    left: 2.6em;
-    top: 3.6em;
-  }
-
-  .eye {
-    position: absolute;
-    background-color: black;
-    width: 1.2em;
-    height: 1.9em;
-    border-radius: 100%;
-    animation: eyes-blink 4.2s infinite alternate;
-
-    &.left {
-      left: 0em;
-    }
-
-    &.right {
-      left: 2.8em;
-    }
-  }
-
-  .mouth {
-    position: absolute;
-    background-color: black;
-    top: 7em;
-    left: 3.2em;
-    width: 3.2em;
-    height: 0.2em;
-    border-radius: 2.2em 2.2em 2em 2em;
-    animation: mounth-shape 2s infinite alternate;
   }
 
   @keyframes eyes-blink {
