427adeaee35090fc1e58a5a2fb3664fc0a90f8f3
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-12-17 01:29:19 +0900

    Add stylelint-config-pretty-order to devDependencies

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 export default {
-	extends: ['stylelint-config-standard', 'stylelint-config-html'],
+	extends: ['stylelint-config-standard', 'stylelint-config-html', 'stylelint-config-pretty-order'],
 	rules: {
 		'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: 'global' }]
 	}
