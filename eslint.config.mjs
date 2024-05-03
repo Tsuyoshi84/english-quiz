1eae05f4de46cf15b18bdf8c622775f6d4bb88c2
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2024-05-03 12:37:26 +0900

    Bump the lint-formatter group with 5 updates

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import tsEslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tsEslint.config(
+	js.configs.recommended,
+	...tsEslint.configs.recommended,
+	...eslintPluginSvelte.configs['flat/recommended'],
+	eslintConfigPrettier,
+	...eslintPluginSvelte.configs['flat/prettier'],
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: { ...globals.node, ...globals.browser },
+			parser: svelteParser,
+			parserOptions: {
+				parser: tsEslint.parser,
+				extraFileExtensions: ['.svelte'],
+				project: './tsconfig.json'
+			}
+		}
+	},
+	{
+		ignores: [
+			'**/.svelte-kit',
+			'**/.vercel',
+			'**/.yarn',
+			'**/build',
+			'**/node_modules',
+			'**/package'
+		]
+	}
+);
