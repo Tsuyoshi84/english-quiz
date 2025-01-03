f498c07358e0ecaebfcdb135c469b40abd4e7b96
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-01-03 01:20:25 +0000

    fix: Fix port forward setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+	server: {
+		host: '0.0.0.0', // Allow external connections
+		port: 5173,
+		strictPort: true // Fail if port is already in use
 	}
 });
