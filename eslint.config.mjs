8d2aa334ca36df1cdbdb4fa16232502a55d93f28
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-04-16 08:34:41 +0900

    Update ESLint packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,48 @@
 import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginSvelte from 'eslint-plugin-svelte';
-import svelteParser from 'svelte-eslint-parser';
-import tsEslint from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
+import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
 
-export default tsEslint.config(
+export default ts.config(
 	js.configs.recommended,
-	...tsEslint.configs.recommended,
-	...eslintPluginSvelte.configs['flat/recommended'],
-	eslintConfigPrettier,
-	...eslintPluginSvelte.configs['flat/prettier'],
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
 	{
-		files: ['**/*.svelte'],
 		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			globals: { ...globals.node, ...globals.browser },
-			parser: svelteParser,
-			parserOptions: {
-				parser: tsEslint.parser,
-				extraFileExtensions: ['.svelte'],
-				project: './tsconfig.json'
+			globals: {
+				...globals.browser,
+				...globals.node
 			}
 		}
 	},
 	{
-		ignores: [
-			'**/.svelte-kit',
-			'**/.vercel',
-			'**/.yarn',
-			'**/build',
-			'**/node_modules',
-			'**/package'
-		]
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		// See more details at: https://typescript-eslint.io/packages/parser/
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'], // Add support for additional file extensions, such as .svelte
+				parser: ts.parser,
+				// Specify a parser for each language, if needed:
+				// parser: {
+				//   ts: ts.parser,
+				//   js: espree,    // Use espree for .js files (add: import espree from 'espree')
+				//   typescript: ts.parser
+				// },
+
+				// We recommend importing and specifying svelte.config.js.
+				// By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
+				// While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
+				// explicitly specifying it ensures better compatibility and functionality.
+				svelteConfig
+			}
+		}
+	},
+	{
+		rules: {
+			// Override or add rule settings here, such as:
+			// 'svelte/rule-name': 'error'
+		}
 	}
 );
