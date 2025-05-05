93711ca5463c69cab735770b9bed46eb90289b78
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-05-05 14:31:32 +0900

    feat: Add animation (#334)
    * feat: Add motion library dependency
    
    This commit adds the `motion` library as a dependency in `package.json` and updates `pnpm-lock.yaml` accordingly. This addition enhances the project's capabilities for animations and transitions.
    
    * feat: Enable TypeScript plugin for Svelte in VSCode settings
    
    This commit adds the configuration option "svelte.enable-ts-plugin" to the VSCode settings.json file, enhancing TypeScript support for Svelte development.
    
    * Add animation

--- src/routes/PhraseCardPager.svelte
+++ src/routes/PhraseCardPager.svelte
@@ -1,65 +1,41 @@
 <script lang="ts">
-	import { fly, scale } from 'svelte/transition';
-	import { quintOut } from 'svelte/easing';
 	import PhraseCard from './PhraseCard.svelte';
 	import type { Phrase } from '../types.ts';
+	import { animate } from 'motion';
 
 	interface Props {
 		/** Phrase to show */
 		phrase: Phrase;
-		/** Whether to show the next card */
-		next: boolean;
+		/** Function to call when the next card is clicked */
+		next: () => void;
 	}
-	let { phrase, next = true }: Props = $props();
+	let { phrase, next }: Props = $props();
 
-	const duration = 500;
+	let phrase_card: HTMLElement;
 
-	function appear(node: Element) {
-		if (next) {
-			return fly(node, { y: 600, opacity: 1, duration });
-		} else {
-			return scale(node, {
-				opacity: 0.5,
-				start: 0.5,
-				easing: quintOut,
-				duration
-			});
-		}
-	}
-
-	function disappear(node: Element) {
-		if (next) {
-			return scale(node, {
-				opacity: 0.5,
-				start: 0.5,
-				easing: quintOut,
-				duration
-			});
-		} else {
-			return fly(node, { y: 600, opacity: 1, duration });
-		}
+	async function show_next_card() {
+		await animate(phrase_card, { opacity: 0, y: [0, -50] }, { duration: 0.2 });
+		next();
+		await animate(phrase_card, { opacity: 1, y: [50, 0] }, { duration: 0.2 });
 	}
 </script>
 
 <div class="card-container">
-	<div id="card-holder-1" class="card-holder" in:appear out:disappear>
-		<PhraseCard {phrase} on:next />
+	<div class="card-holder" bind:this={phrase_card}>
+		<PhraseCard {phrase} next={show_next_card} />
 	</div>
 </div>
 
 <style>
 	.card-container {
 		position: relative;
+		display: flex;
 		inline-size: 100%;
 		block-size: 100%;
-	}
+		align-items: center;
 
-	.card-holder {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		inline-size: 100%;
-		margin: auto;
-		transform: translate(-50%, -50%);
+		.card-holder {
+			inline-size: 100%;
+		}
 	}
 </style>
